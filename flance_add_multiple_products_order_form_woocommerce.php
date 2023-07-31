95b6467b7baeeba44e04d68451e612cebdf2840e
BK <akhmedov@balcomsoft.com>
2023-08-01 00:23:06 +0500

    116

--- flance_add_multiple_products_order_form_woocommerce.php
+++ flance_add_multiple_products_order_form_woocommerce.php
@@ -15,7 +15,7 @@
  * @wordpress-plugin
  * Plugin Name:       Flance Add Multiple Products order form for Woocommerce
  * Description:       The plugin gives functionality to have the form to add multiple products to the cart and calculate in same page the total price of the order. And you also can use shortcode to use the plugin other places. Just place the shortcode where you wanna put the input form and it's done !!! Pro vesrion has the functionality to show the product attributes on the page non commercial version does not have attribute show functionality. 
- * Version:           1.1.5
+ * Version:           1.1.6
  * Author:            Rusty 
  * Author URI:        http://www.flance.info
  * Text Domain:       flance-add-multiple-products-order-form-woocommerce
